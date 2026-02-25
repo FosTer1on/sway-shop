a74bc0b024a4f274274d61d8afabdcbe35fa7b7b
FosTer1on <shaha.robot.848@gmail.com>
2026-02-26 00:18:40 +0500

    Delete last name from Required fields in user models

--- apps/user/models.py
+++ apps/user/models.py
@@ -32,7 +32,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'phone_number'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = ['first_name']
 
     def __str__(self):
         return self.phone_number
